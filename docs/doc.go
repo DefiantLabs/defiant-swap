bd7093dfe4b6e6a61d42fad5e2a584e0d941f6b0
kyle <moser.kyle@gmail.com>
2022-11-30 07:59:55 -0500

    make swagger && make server-swagger to view docs

---
+++ docs/doc.go
@@ -0,0 +1,24 @@
+// Package classification defiant-swap.
+//
+// Estimate user swaps ahead of time, and provide arbitrage trade information.
+//
+//	 Schemes: http
+//	 BasePath: /
+//	 Version: 1.0.0
+//	 Host: arb.defiantlabs.net:8080
+//
+//	 Consumes:
+//	 - application/json
+//
+//	 Produces:
+//	 - application/json
+//
+//	 Security:
+//	 - basic
+//
+//	SecurityDefinitions:
+//	basic:
+//	  type: basic
+//
+// swagger:meta
+package docs
